ee5e15a15db3abf8a79a2b7f98db7b25cba37050
uglyhurd <dolgivlad@icloud.com>
2025-12-28 02:58:20 +0300

    aaa

--- src/main/java/com/example/Security/Controllers/HelloController.java
+++ src/main/java/com/example/Security/Controllers/HelloController.java
@@ -3,15 +3,13 @@ package com.example.Security.Controllers;
 import com.example.Security.Repositories.PersonRepositories;
 import com.example.Security.Security.PersonDetails;
 import com.example.Security.Services.AdminService;
-import org.apache.tomcat.util.collections.CaseInsensitiveKeyMap;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloController {
@@ -37,7 +35,19 @@ public class HelloController {
     public String adminPage(Model model){
         model.addAttribute("people", personRepositories.findAll());
         adminService.doAdminStuff();
-        return "adminPage";
+        return "adminPages/adminPage";
+    }
+
+    @GetMapping("/menu")
+    public String mainMenu(Model model){
+        return "UserPages/menuPage";
+    }
+
+    @GetMapping("adminMenu")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public String adminMenu(Model model){
+        model.addAttribute("people", personRepositories.findAll());
+        return "adminPages/adminMenu";
     }
 
 }
